c0aec658f40093a5e13f142db29ee03e4cec59eb
Cole Shelly <Cole Shelly>
2017-07-17 15:57:42 -0700

    worked more on zombies

--- zmash_test/src/edu/digipen/coleshelly/Level1.java
+++ zmash_test/src/edu/digipen/coleshelly/Level1.java
@@ -3,6 +3,7 @@ package edu.digipen.coleshelly;
 import edu.digipen.gameobject.GameObject;
 import edu.digipen.gameobject.ObjectManager;
 import edu.digipen.level.GameLevel;
+import edu.digipen.math.PFRandom;
 
 /**
  * Created by jake.ostrow on 7/17/2017.
@@ -12,6 +13,8 @@ public class Level1 extends GameLevel
 
 	@Override public void create()
 	{
+		for (int i = 0; i < 1000; ++i)
+		{
 		// Add car to level
 		GameObject car = new Car();
 		ObjectManager.addGameObject(car);
--- zmash_test/src/edu/digipen/coleshelly/Zombie.java
+++ zmash_test/src/edu/digipen/coleshelly/Zombie.java
@@ -2,6 +2,7 @@ package edu.digipen.coleshelly;
 
 import edu.digipen.gameobject.GameObject;
 import edu.digipen.gameobject.ObjectManager;
+import edu.digipen.math.PFRandom;
 import edu.digipen.math.Vec2;
 
 /**
@@ -10,6 +11,14 @@ import edu.digipen.math.Vec2;
 
 public class Zombie extends GameObject
 {
+	// Maximum Health
+	public int MaxHealth = 10;
+
+	// Current Health
+	public int ZombieHealth = 1;
+
+
+
 	public Zombie()
 	{
 		// Call the base constructor
@@ -18,6 +27,9 @@ public class Zombie extends GameObject
 
 	@Override public void initialize()
 	{
+		ZombieHealth = MaxHealth;
+
+		setPosition(0, 0);
 	}
 
 	@Override
@@ -38,4 +50,72 @@ public class Zombie extends GameObject
 		this.setPositionY(this.getPositionY() + vector.getY());
 	}
 
+	boolean checkRectangleRectangleCollision(Vec2 r1Position, float r1HalfWidth,
+			float r1HalfHeight, Vec2 r2Position, float r2HalfWidth, float r2HalfHeight)
+	{
+		float r1LeftCoordinate = r1Position.getX() + r1HalfWidth;
+		//Left: Check if r1's right side is further left than r2's left side
+		//If it is, there can be no collision, so return false.
+		float r2RightCoordinate = r2Position.getX() - r2HalfWidth;
+		//Right: Check if r1's left side is further right than r2's right side
+		//If it is, there can be no collision, so return false.
+		if (r1LeftCoordinate < r2RightCoordinate)
+		{
+			return false;
+		}
+
+		float r1RightCoordinate = r1Position.getX() - r1HalfWidth;
+
+		float r2LeftCoordinate = r2Position.getX() + r2HalfWidth;
+
+		if (r1RightCoordinate > r2LeftCoordinate)
+		{
+			return false;
+		}
+		//Bottom: Check if r1's top side is below r2's bottom side
+		//If it is, there can be no collision, so return false.
+		float r1BottomCoordinate = r1Position.getY() + r1HalfHeight;
+
+		float r2TopCoordinate = r2Position.getY() - r2HalfHeight;
+
+		if (r1BottomCoordinate < r2TopCoordinate)
+		{
+			return false;
+		}
+		//Top: Check if r1's bottom side is above r2's top side
+		//If it is, there can be no collision, so return false.
+		float r1TopCoordinate = r1Position.getY() - r1HalfHeight;
+
+		float r2BottomCoordinate = r2Position.getY() + r2HalfHeight;
+
+		if (r1TopCoordinate > r2BottomCoordinate)
+		{
+			return false;
+		}
+		//If none of those conditions are true, return true, as anything left is a collision.
+
+		// DELETE THIS LINE OF CODE WHEN YOU ARE READY!! This is just a place
+		// holder so that your program will compile!!
+		return true;
+	}
+
+	public void applydamage(int damage)
+	{
+		if (damage > 0)
+		{
+			ZombieHealth -= damage;
+		}
+	}
+
+	@Override
+	public void collisionReaction(GameObject collidedWith)
+	{
+		float x = PFRandom.randomRange(-3, 3);
+		float y = PFRandom.randomRange(-3, 3);
+
+		this.setPositionX(this.getPositionX() + x);
+		this.setPositionY(this.getPositionY() + y);
+	}
 }
+
+
