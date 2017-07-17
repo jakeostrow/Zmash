69cbc751d78ac70a6101d61851f428b6d8d51ed4
Jake Ostrow <Jake Ostrow>
2017-07-17 11:28:03 -0700

    jake's third commit

---
+++ zmash_test/src/edu/digipen/coleshelly/Car.java
@@ -0,0 +1,16 @@
+package edu.digipen.coleshelly;
+
+import edu.digipen.gameobject.GameObject;
+import edu.digipen.math.Vec2;
+
+/**
+ * Created by jake.ostrow on 7/17/2017.
+ */
+public class Car extends GameObject
+{
+	public Car(Vec2 position)
+	{
+		super("Car", 20, 20, "square.png");
+	}
+
+}
---
+++ zmash_test/src/edu/digipen/coleshelly/Level1.java
@@ -0,0 +1,30 @@
+package edu.digipen.coleshelly;
+
+import edu.digipen.level.GameLevel;
+
+/**
+ * Created by jake.ostrow on 7/17/2017.
+ */
+public class Level1 extends GameLevel
+{
+
+	@Override public void create()
+	{
+
+	}
+
+	@Override public void initialize()
+	{
+
+	}
+
+	@Override public void update(float v)
+	{
+
+	}
+
+	@Override public void uninitialize()
+	{
+
+	}
+}
