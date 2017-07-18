79ae05586cc8fd779902ec14dc1ba1e4024be443
Jake Ostrow <Jake Ostrow>
2017-07-18 11:31:32 -0700

    change made

--- zmash_test/src/edu/digipen/coleshelly/Level1.java
+++ zmash_test/src/edu/digipen/coleshelly/Level1.java
@@ -60,19 +60,5 @@ public class Level1 extends GameLevel
 
 	}
 
-	@Override public void initialize()
-	{
-
-	}
-
-	@Override public void update(float v)
-	{
-
-	}
-
-	@Override public void uninitialize()
-	{
-
-	}
 }
 
